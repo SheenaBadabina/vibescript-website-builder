35ebb2c72f4bfe14e49dd95d45cbc06ae4f4c1a6
SheenaBadabina <knowlessknowledge@gmail.com>
2025-09-15 05:45:15 -0600

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+// src/app/layout.tsx
+import "./globals.css";
+import { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <SessionProvider>{children}</SessionProvider>
+      </body>
+    </html>
+  );
+}
