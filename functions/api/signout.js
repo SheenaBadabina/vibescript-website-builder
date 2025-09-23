5a9b50af7407d34ec0106f760197029dc794e7b4
SheenaBadabina <knowlessknowledge@gmail.com>
2025-09-22 18:37:55 -0600

    Update signout.js

--- functions/api/signout.js
+++ functions/api/signout.js
@@ -1,8 +1,5 @@
-// functions/api/signout.js
-import { clearCookieHeader } from "../_utils/session.js";
-
-export function onRequest({ request }) {
-  const headers = new Headers({ "Set-Cookie": clearCookieHeader() });
-  headers.set("Location", "/signin");
-  return new Response(null, { status: 302, headers });
+import { COOKIE } from "../_utils/session.js";
+export async function onRequest() {
+  const h = new Headers(); h.set("Set-Cookie", `${COOKIE}=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Lax`);
+  h.set("Location","/signin"); return new Response(null,{status:302, headers:h});
 }
