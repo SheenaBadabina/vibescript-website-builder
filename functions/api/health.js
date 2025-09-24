6f10d7b597836b649c1b714f6e50592dbbcc4234
SheenaBadabina <knowlessknowledge@gmail.com>
2025-09-24 05:55:59 -0600

    Update health.js

--- functions/api/health.js
+++ functions/api/health.js
@@ -1,6 +1,4 @@
-export async function onRequestGet({ env }) {
-  const ok = !!(env && env.USERS && env.VERIFY);
-  return new Response(JSON.stringify({ ok }), {
-    headers: { "content-type": "application/json", "cache-control":"no-store" }
+export const onRequestGet = () =>
+  new Response(JSON.stringify({ ok: true, service: "pages-functions" }), {
+    headers: { "content-type": "application/json; charset=utf-8" },
   });
-}
